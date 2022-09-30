4e88cd686ee8c788ea71ba61c6256f8f4383076f
Bryan <71095749+bryanless@users.noreply.github.com>
2022-09-30 12:45:29 +0700

    Mission 1

---
+++ lib/theme/shape.dart
@@ -0,0 +1,4 @@
+class YumeShape {
+  static const double roundedRectangle = 6;
+  static const double circle = 50;
+}
---
+++ lib/theme/space.dart
@@ -0,0 +1,5 @@
+class YumeSpace {
+  static const double small = 8;
+  static const double medium = 16;
+  static const double large = 24;
+}
